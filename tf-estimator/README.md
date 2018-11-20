ec0ffef747c318558e7c9dfccb67fa331c63cb60
Adrian Yi <sjay87@gmail.com>
2018-11-20 15:45:40 -0500

    Clean up code & text

--- tf-estimator/README.md
+++ tf-estimator/README.md
@@ -3,9 +3,6 @@
 <p align="center">
 <img src="../co_logo.png" alt="Clusterone" width="200">
 <br>
-<br>
-<a href="https://slackin-altdyjrdgq.now.sh"><img src="https://slackin-altdyjrdgq.now.sh/badge.svg" alt="join us on slack"></a>
-</p>
 
 This is a tutorial on how to use TensorFlow's [Estimator class](https://www.tensorflow.org/api_docs/python/tf/estimator/Estimator), including creating an Estimator by importing a Keras model. The code uses the [MNIST](http://yann.lecun.com/exdb/mnist/) dataset.
 
@@ -26,19 +23,17 @@ To run the code, you need:
 
 - [Python](https://python.org/) 3.6
 - [Git](https://git-scm.com/)
-- TensorFlow 1.5. Install it like this: `pip install tensorflow`
-- [NumPy](http://www.numpy.org/). Get it with `pip install numpy`
+- TensorFlow 1.5 or higher. Install it like this: `pip install tensorflow`
 - The Clusterone Python library. Install it with `pip install clusterone`
+- GitHub account. Create an account for free on [https://github.com/](https://github.com/)
 - To run the code on Clusterone, you need a Clusterone account. [Join the waitlist](https://clusterone.com/join-waitlist/) here.
 
 
 ### Setting Up
 
-Start out by cloning this repository onto your local machine. 
+Follow the **Set Up** section of the [Get Started](https://docs.clusterone.com/get-started#set-up) guide to add your GitHub personal access token to your Clusterone account.
 
-```shell
-git clone https://github.com/clusterone/clusterone-tutorials
-```
+Then follow [Create a project](https://docs.clusterone.com/get-started#create-a-project) section to add clusterone-tutorials project. Use **`clusterone/clusterone-tutorials`** repository instead of what is shown in the guide.
 
 ## Usage
 
@@ -46,47 +41,57 @@ You can run the tutorial code either on your local machine or on the Clusterone 
 
 ### Run the code locally
 
-Make sure you have all requirements installed that are listed above. Assuming all packages are installed correctly, you can run all script with `python mnist.py`. The script will download the mnist dataset and then start training.
+Start out by cloning this repository onto your local machine. 
+
+```shell
+git clone https://github.com/clusterone/clusterone-tutorials
+```
+
+Then navigate to the directory with `cd clusterone-tutorials`.
+
+Make sure you have all requirements installed that are listed above. Assuming all packages are installed correctly, you can run all script with `python mnist.py`. The script will download the mnist dataset and then start training. You can view the training results with Tensorboard with `tensorboard --logdir=logs`.
 
 ### Run on Clusterone
 
 These instructions use the `just` command line tool. It comes with the Clusterone Python library and is installed automatically with the library.
 
-cd into the folder you cloned with `cd clusterone-tutorials/tf-estimator`  and log into your Clusterone account using `just login`.
+If you have used Clusterone library before with a different Clusterone installation, make sure it is connected to the correct endpoint by running `just config endpoint https://clusterone.com`.
 
-First, create a new git repository in the code directory and commit the Python files to it:
+Log into your Clusterone account using `just login`, and entering your login information.
 
+First, let's make sure that you have the project. Execute the command `just get projects` to see all your projects. You should see something like this:
 ```shell
-git init
-git add .
-git commit -m "Initial commit"
+>> just get projects
+All projects:
+
+| # | Project                       | Created at          | Description |
+|---|-------------------------------|---------------------|-------------|
+| 0 | username/clusterone-tutorials | 2018-11-20T00:00:00 |             |
 ```
+where `username` should be your Clusterone account name.
 
-Then, create a new project on Clusterone:
-
+Let's create a job. Make sure to replace `username` with your username.
 ```shell
-just init project tf-estimator
-```
-
-Now, upload the code to the new project:
-
-```shell
-git push clusterone master
-```
-
-Finally, create a job. Make sure to replace `YOUR_USERNAME` with your username.
-
-```shell
-just create job distributed --project YOUR_USERNAME/tf-estimator --module mnist --name first-job \
---time-limit 1h
+just create job distributed \
+  --project username/clusterone-tutorials \
+  --name distributed-mnist-job \
+  --worker-replicas 2 \
+  --worker-type aws-t2-small \
+  --docker-image tensorflow-1.11.0-cpu-py35 \
+  --ps-replicas 1 \
+  --ps-type aws-t2-small \
+  --ps-docker-image tensorflow-1.11.0-cpu-py35 \
+  --time-limit 1h \
+  --command "python tf-estimator/main.py" \
+  --setup_command "pip install -r tf-estimator/requirements.txt"
 ```
 
 This creates a job with 2 worker nodes and 1 parameter server. See our [documentation](https://docs.clusterone.com/cli-reference-documentation/just-create-job) for more information on how to change the number and instance types of worker and parameter servers.
 
-Now all that's left to do is starting the job:
+Now the final step is to start the job:
 
 ```shell
-just start job -p tf-estimator/first-job
+just start job -p clusterone-tutorials/distributed-mnist-job
 ```
 
 That's it! You can monitor its progress on the command line using `just get events`. More elaborate monitoring is available on the [Matrix](https://clusterone.com/matrix), Clusterone's graphical web interface.
@@ -95,7 +100,11 @@ That's it! You can monitor its progress on the command line using `just get even
 
 For further information on this example, take a look at the [tutorial](https://clusterone.com/blog/2018/09/19/distributed-tensorflow-estimator-class) based on this repository on the Clusterone Blog.
 
+For a more updated MNIST example, check out our [MNIST repo](https://github.com/clusterone/mnist). We also have other examples [here](https://docs.clusterone.com/examples).
+
 For further info on the MNIST dataset, check out [Yann LeCun's page](http://yann.lecun.com/exdb/mnist/) about it. To learn more about TensorFlow and Deep Learning in general, take a look at the [TensorFlow](https://tensorflow.org) website.
+
+If you have any further questions, don't hesitate to write us a support ticket on [Clusterone.com](https://clusterone.com) or join us on [Slack](https://bit.ly/2OPc6JH)!
 
 ## License
 
--- tf-estimator/mnist.py
+++ tf-estimator/mnist.py
@@ -1,8 +1,7 @@
 import json
 import os
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-import numpy as np
 import tensorflow as tf
 from clusterone import get_data_path, get_logs_path
 
@@ -10,89 +9,132 @@ from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-# Clusterone snippet 1
-# Creates necessary TF_CONFIG environment variable for Estimator
-try:
-    job_name = os.environ['JOB_NAME']
-    task_index = int(os.environ['TASK_INDEX'])
-    ps_hosts = os.environ['PS_HOSTS'].split(',')
-    worker_hosts = os.environ['WORKER_HOSTS'].split(',')
-    TF_CONFIG = {'task': {'type': job_name, 'index': task_index},
-                 'cluster': {'chief': [worker_hosts[0]],
-                             'worker': worker_hosts,
-                             'ps': ps_hosts},
-                 'environment': 'cloud'}
-    local_ip = 'localhost:' + TF_CONFIG['cluster'][job_name][task_index].split(':')[1]
-    if (job_name == 'chief') or (job_name == 'worker' and task_index == 0):
-        job_name = 'chief'
-        TF_CONFIG['task']['type'] = 'chief'
-        TF_CONFIG['cluster']['worker'][0] = local_ip
-    TF_CONFIG['cluster'][job_name][task_index] = local_ip
-    os.environ['TF_CONFIG'] = json.dumps(TF_CONFIG)
-except KeyError as ex:
-    pass
 
 def get_args():
-    '''Return parsed args'''
-    parser = ArgumentParser()
+    """Parse arguments"""
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter,
+                            description='''Train a convolution neural network with MNIST dataset.
+                            For distributed mode, the script will use few environment variables as defaults:
+                            JOB_NAME, TASK_INDEX, PS_HOSTS, and WORKER_HOSTS. These environment variables will be
+                            available on distributed Tensorflow jobs on Clusterone platform by default.
+                            If running this locally, you will need to set these environment variables
+                            or pass them in as arguments (i.e. python mnist.py --job_name worker --task_index 0
+                            --worker_hosts "localhost:2222,localhost:2223" --ps_hosts "localhost:2224").
+                            If these are not set, the script will run in non-distributed (single instance) mode.''')
+
+    # Configuration for distributed task
+    parser.add_argument('--job_name', type=str, default=os.environ.get('JOB_NAME', None), choices=['worker', 'ps'],
+                        help='Task type for the node in the distributed cluster. Worker-0 will be set as master.')
+    parser.add_argument('--task_index', type=int, default=os.environ.get('TASK_INDEX', 0),
+                        help='Worker task index, should be >= 0. task_index=0 is the chief worker.')
+    parser.add_argument('--ps_hosts', type=str, default=os.environ.get('PS_HOSTS', ''),
+                        help='Comma-separated list of hostname:port pairs.')
+    parser.add_argument('--worker_hosts', type=str, default=os.environ.get('WORKER_HOSTS', ''),
+                        help='Comma-separated list of hostname:port pairs.')
+
+    # Experiment related parameters
     parser.add_argument('--local_data_dir', type=str, default='data/',
                         help='Path to local data directory')
     parser.add_argument('--local_log_dir', type=str, default='logs/',
                         help='Path to local log directory')
-    # Model params
-    parser.add_argument('--hidden_units', type=int, nargs='*', default=[32, 64])
-    parser.add_argument('--learning_rate', type=float, default=0.001)
-    parser.add_argument('--learning_decay', type=float, default=0.001)
-    parser.add_argument('--dropout', type=float, default=0.5)
-    parser.add_argument('--batch_size', type=int, default=512)
+
+    # Training params
+    parser.add_argument('--learning_rate', type=float, default=0.001,
+                        help='Initial learning rate used in Adam optimizer.')
+    parser.add_argument('--learning_decay', type=float, default=0.001,
+                        help='Exponential decay rate of the learning rate per step.')
+    parser.add_argument('--batch_size', type=int, default=512,
+                        help='Batch size to use during training and evaluation.')
     opts = parser.parse_args()
 
-    # Clusterone snippet 2
-    # Grabs the correct paths, depending on if the job is running local or on Clusterone
-    opts.data_dir = get_data_path(dataset_name = '',
-                                 local_root = opts.local_data_dir,
-                                 local_repo = '',
-                                 path = '')
-    opts.log_dir = get_logs_path(root = opts.local_log_dir)
+    # Clusterone snippet: Grabs the correct paths, depending on if the job is running local or on Clusterone
+    opts.data_dir = get_data_path(dataset_name='',
+                                  local_root=opts.local_data_dir,
+                                  local_repo='',
+                                  path='')
+    opts.log_dir = get_logs_path(root=opts.local_log_dir)
 
     return opts
 
-def get_model(opts):
-    '''Return a CNN Keras model'''
+
+def make_tf_config(opts):
+    """
+    Returns TF_CONFIG that can be used to set the environment variable necessary for distributed training
+    """
+    if all([opts.job_name is None, not opts.ps_hosts, not opts.worker_hosts]):
+        return {}
+    elif any([opts.job_name is None, not opts.ps_hosts, not opts.worker_hosts]):
+        tf.logging.warn('Distributed setting is incomplete. You must pass job_name, ps_hosts, and worker_hosts.')
+        if opts.job_name is None:
+            tf.logging.warn('Expected job_name of worker or ps. Received {}.'.format(opts.job_name))
+        if not opts.ps_hosts:
+            tf.logging.warn('Expected ps_hosts, list of hostname:port pairs. Got {}. '.format(opts.ps_hosts) +
+                            'Example: --ps_hosts "localhost:2224" or --ps_hosts "localhost:2224,localhost:2225')
+        if not opts.worker_hosts:
+            tf.logging.warn('Expected worker_hosts, list of hostname:port pairs. Got {}. '.format(opts.worker_hosts) +
+                            'Example: --worker_hosts "localhost:2222,localhost:2223"')
+        tf.logging.warn('Ignoring distributed arguments. Running single mode.')
+        return {}
+
+    tf_config = {
+        'task': {
+            'type': opts.job_name,
+            'index': opts.task_index
+        },
+        'cluster': {
+            'master': [opts.worker_hosts[0]],
+            'worker': opts.worker_hosts,
+            'ps': opts.ps_hosts
+        },
+        'environment': 'cloud'
+    }
+
+    # Nodes may need to refer to itself as localhost
+    local_ip = 'localhost:' + tf_config['cluster'][opts.job_name][opts.task_index].split(':')[1]
+    tf_config['cluster'][opts.job_name][opts.task_index] = local_ip
+    if opts.job_name == 'worker' and opts.task_index == 0:
+        tf_config['task']['type'] = 'master'
+        tf_config['cluster']['master'][0] = local_ip
+    return tf_config
+
+
+def keras_model(opts):
+    """Return a CNN Keras model"""
     input_tensor = tf.keras.layers.Input(shape=(784,), name='input')
 
     temp = tf.keras.layers.Reshape([28, 28, 1], name='input_image')(input_tensor)
-    for i, n_units in enumerate(opts.hidden_units):
+    for i, n_units in enumerate([32, 64]):
         temp = tf.keras.layers.Conv2D(n_units, kernel_size=3, strides=(2, 2),
                                       activation='relu', name='cnn'+str(i))(temp)
-        temp = tf.keras.layers.Dropout(opts.dropout, name='dropout'+str(i))(temp)
+        temp = tf.keras.layers.Dropout(0.5, name='dropout'+str(i))(temp)
     temp = tf.keras.layers.GlobalAvgPool2D(name='average')(temp)
     output = tf.keras.layers.Dense(10, activation='softmax', name='output')(temp)
 
     model = tf.keras.models.Model(inputs=input_tensor, outputs=output)
     optimizer = tf.keras.optimizers.Adam(lr=opts.learning_rate, decay=opts.learning_decay)
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     print(model.summary())
     return model
 
+
 def main(opts):
-    '''Main function'''
+    """Main function"""
     data = read_data_sets(opts.data_dir,
                           one_hot=False,
                           fake_data=False)
 
-    model = get_model(opts)
+    model = keras_model(opts)
     config = tf.estimator.RunConfig(
                 model_dir=opts.log_dir,
                 save_summary_steps=1,
                 save_checkpoints_steps=100,
                 keep_checkpoint_max=3,
                 log_step_count_steps=10)
-    classifier = tf.keras.estimator.model_to_estimator(model, model_dir=opts.log_dir, config=config)
+    estimator = tf.keras.estimator.model_to_estimator(model, model_dir=opts.log_dir, config=config)
 
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
                          x={'input': data.train.images},
-                         y=tf.keras.utils.to_categorical(data.train.labels.astype(np.int32), 10).astype(np.float32),
+                         y=data.train.labels,
                          num_epochs=None,
                          batch_size=opts.batch_size,
                          shuffle=True,
@@ -100,7 +142,7 @@ def main(opts):
                          num_threads=4)
     eval_input_fn = tf.estimator.inputs.numpy_input_fn(
                          x={'input': data.test.images},
-                         y=tf.keras.utils.to_categorical(data.test.labels.astype(np.int32), 10).astype(np.float32),
+                         y=data.test.labels,
                          num_epochs=1,
                          shuffle=False,
                          queue_capacity=10*opts.batch_size,
@@ -113,8 +155,14 @@ def main(opts):
                                       start_delay_secs=0,
                                       throttle_secs=60)
 
-    tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
 
 if __name__ == '__main__':
     opts = get_args()
+
+    # Clusterone snippet: Set environment variable TF_CONFIG for distributed training
+    TF_CONFIG = make_tf_config(opts)
+    os.environ['TF_CONFIG'] = json.dumps(TF_CONFIG)
+
     main(opts)
